d738a0a606b65a2a92eb243f290abd62b79d79b6
Nathaniel Xu <nxu981@gmail.com>
2023-11-15 12:05:29 -0500

    Initial file structure
    Co-authored-by: Joshua Lindsay <Joshwlindsay@gmail.com>
    Co-authored-by: Steve Price <sgp1229@gmail.com>
    Co-authored-by: Zachary Kerner <zachkornergih@gmail.com>

---
+++ terraform/elasticIP/elasticIP.tf
@@ -0,0 +1,26 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16" 
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_eip" "my_eip" {
+  vpc = false
+}
+
+output "eip_address" {
+  value = aws_eip.my_eip.public_ip
+}
+
+output "eip_allocation_id" {
+  value = aws_eip.my_eip.allocation_id
+}
